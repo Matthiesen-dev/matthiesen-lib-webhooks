85bc6bb1b96d81efea023416229a374e0a3a2cfb
Adam Matthiesen <amatthiesen@outlook.com>
2026-08-04 17:57:36 -0700

    Update Gradle configuration to include new runtime dependencies and version updates

--- gradle/build-logic/src/main/kotlin/matthiesen.project-conventions.gradle.kts
+++ gradle/build-logic/src/main/kotlin/matthiesen.project-conventions.gradle.kts
@@ -36,6 +36,9 @@ repositories {
             includeGroup("dev.matthiesen")
         }
     }
+    maven("https://raw.githubusercontent.com/Fuzss/modresources/main/maven/") {
+        name = "Fuzs Mod Resources"
+    }
     // for development builds
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
         name = "sonatype-oss-snapshots1"
